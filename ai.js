9158287589362543677a274e971c75995eded9b3
RIAEvangelist <brandon@diginow.it>
2024-01-10 16:34:38 -0800

    patch for model

--- ai.js
+++ ai.js
@@ -38,7 +38,7 @@ async function process_prompt(prompt){
     const stream = await openai.chat.completions.create(
         {
             //model: 'gpt-3.5-turbo-1106',
-            model: 'gpt-4-turbo-1106',
+            model: 'gpt-4-1106-preview',
             messages: messages,
             stream: true,
         }
