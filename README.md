da0b84320c298ca6d0de58f43f8014fcf322aec3
RIAEvangelist <brandon@diginow.it>
2024-01-10 16:07:16 -0800

    initial working module

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # ai-cli
+
+## setup
+make a new dir  
+inside of that new dir clone the `ai-cli` repo  
+in the new dir, ***NOT*** the `ai-cli` repo, make a `package.json` file and a `keys.js` file and put your API key in it. It is in the git ignore, but still, put it in the parent Dir of ai-cli, not inside of `ai-cli`.
+
+### ../keys.js file
+```js
+
+export const OPENAI_API_KEY='your-key-example';
+export default OPENAI_API_KEY;
+
+```
+
+### ../package.json file
+```js
+
+{
+  "type":"module"
+}
+
+```
+
+
+
+### Run it
+from inside the `ai-cli` dir run the following.
+
+```bash
+
+npm i
+npm start
+
+```
---
+++ ai.js
@@ -0,0 +1,67 @@
+//core
+import {default as readline} from 'readline';
+
+//internal
+import OPENAI_API_KEY from '../keys.js';
+
+//external
+import 'openai/shims/web';
+import OpenAI from 'openai';
+
+const openai = new OpenAI(
+    {
+        apiKey: OPENAI_API_KEY
+    }
+);
+
+const rl = readline.createInterface(
+    { 
+        input:process.stdin, 
+        output:process.stdout 
+    }
+);
+
+const messages=[];
+
+async function main() {
+    rl.question(
+        'YOU> ',
+        process_prompt
+    );
+}
+
+async function process_prompt(prompt){
+    messages.push(
+        { role: 'user', content: prompt }
+    );
+
+    const stream = await openai.chat.completions.create(
+        {
+            //model: 'gpt-3.5-turbo-1106',
+            model: 'gpt-4-turbo-1106',
+            messages: messages,
+            stream: true,
+        }
+    );
+    
+    let resp='';
+
+    console.log('\nGPT:\n');
+
+    for await (const chunk of stream) {
+        const data=chunk.choices[0]?.delta?.content || '';
+
+        process.stdout.write(data);
+        resp+=data;
+    }
+
+    messages.push(
+        { role: 'assistant', content: resp }
+    );
+
+    console.log('\n');
+
+    main();
+}
+
+main();
